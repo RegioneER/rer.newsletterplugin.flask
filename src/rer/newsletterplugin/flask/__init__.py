cc005fb1233799157c463f59860579571231903b
Giacomo Monari <arsenico13@hotmail.it>
2020-04-10 11:48:07 +0200

    [new] flask_adapter to send newsletters outside Plone (wip)

--- src/rer/newsletterplugin/flask/__init__.py
+++ src/rer/newsletterplugin/flask/__init__.py
@@ -2,5 +2,7 @@
 """Init and utils."""
 from zope.i18nmessageid import MessageFactory
 
+import logging
 
 _ = MessageFactory('rer.newsletterplugin.flask')
+logger = logging.getLogger('rer.newsletterplugin.flask')
