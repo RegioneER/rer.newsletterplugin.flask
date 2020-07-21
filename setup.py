982fcc07401c7ec881c39e9f758b66b1e5130771
Andrea Cecchi <andrea.cecchi85@gmail.com>
2020-07-21 09:49:31 +0200

    Preparing release 0.1.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = '\n\n'.join(
 
 setup(
     name='rer.newsletterplugin.flask',
-    version='1.0a1',
+    version='0.1.0',
     description="Add-on per Plone collegato a rer.newsletter per inviare le mail usando un servizio Flask",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
