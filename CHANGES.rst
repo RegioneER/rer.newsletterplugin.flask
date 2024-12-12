5141e501a5b963e0dab5b564786092051acb3e7c
Andrea Cecchi <andrea.cecchi85@gmail.com>
2024-12-12 10:48:41 +0100

    fix python version

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,11 @@
 Changelog
 =========
 
+0.1.0.py27 (2024/12/12)
+-----------------------
+
+- Fix python version dependency for python2.7
+  [cekk]
 
 0.1.0 (2020-07-21)
 ------------------
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = '\n\n'.join(
 
 setup(
     name='rer.newsletterplugin.flask',
-    version='0.1.0',
+    version='0.1.0.py27',
     description="Add-on per Plone collegato a rer.newsletter per inviare le mail usando un servizio Flask",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
@@ -48,7 +48,7 @@ setup(
     package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
-    python_requires="==2.7, >=3.6",
+    python_requires="==2.7",
     install_requires=[
         'setuptools',
         # -*- Extra requirements: -*-
